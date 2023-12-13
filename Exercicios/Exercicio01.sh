db59a48af066c4935b85867c8b3c28d09aed7f8c
JVSMOTA <jvmota.sb@gmail.com>
2023-12-13 13:51:27 -0300

    :rocket: feat: primeiro script em bash

---
+++ Exercicios/Exercicio01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#==============================================================================#
+# Author: João Mota < jvmota.sb@gmail.com >                                    #
+# GitHub: https://github.com/JVSMOTA                                           #
+# Computer Science undergraduate at UFCG                                       #
+#==============================================================================#
+
+# Created: 13/12/2023
+# Time of Creation: 13:38:49
+#
+# Question: Crie um script que declare três variáveis: cidade, estado e país, e atribua valores a elas representando sua localização atual.
+
+cidade="Serra Branca"
+estado="Paraíba"
+país="Brasil"
