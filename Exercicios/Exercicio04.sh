ebe901de9ff0e22122124dd471de9e3802f915d7
JVSMOTA <jvmota.sb@gmail.com>
2023-12-13 14:11:36 -0300

    :rocket: feat: script que entra em loop for

---
+++ Exercicios/Exercicio04.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#==============================================================================#
+# Author: João Mota < jvmota.sb@gmail.com >                                    #
+# GitHub: https://github.com/JVSMOTA                                           #
+# Computer Science undergraduate at UFCG                                       #
+#==============================================================================#
+
+# Created: 13/12/2023
+# Time of Creation: 14:07:56
+#
+# Question: Crie um novo script que utilize um loop for para imprimir os quadrados dos números de 1 a 5.
+
+for i in {1..5}; do
+    echo "Número: $i"
+done
